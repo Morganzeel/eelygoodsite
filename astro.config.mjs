39c1c1f387772270dadbd6fe8c72209e66c1702d
Morganzeel <186287331+Morganzeel@users.noreply.github.com>
2025-12-21 21:15:12 +0100

    test for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from '@astrojs/cloudflare';
 
 import icon from "astro-icon";
 
@@ -9,6 +10,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
+  adapter: cloudflare(),
   integrations: [icon()],
 });
